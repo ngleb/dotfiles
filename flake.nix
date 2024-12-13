c4dc27eaf955090f4536fdc5f2338e6a41cb46eb
Gleb Nemshilov <kirokko@gmail.com>
2024-12-13 22:27:25 +0700

    updates

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Gleb's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     agenix = {
       url = "github:ryantm/agenix";
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
