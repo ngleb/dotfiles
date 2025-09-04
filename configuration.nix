50ce5a1e52f27c453dda31ef6e7a8691f592a049
Gleb Nemshilov <kirokko@gmail.com>
2025-09-04 16:34:36 +0700

    updates

--- configuration.nix
+++ configuration.nix
@@ -80,7 +80,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   hardware.bluetooth = {
     enable = true;
