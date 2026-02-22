d325cb31b493b09d117e2b52058c7a9bd81c48b0
Gleb Nemshilov <kirokko@gmail.com>
2026-02-22 23:34:18 +0700

    freeze linux 6.18 version

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
 
   hardware.bluetooth = {
     enable = true;
